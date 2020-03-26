e11189435d54d682cc049d68b681d759fe8502e4
wander.porto <wander.porto@ernetwork.com.br>
2020-03-26 10:58:06 -0300

    subindo backend

---
+++ backend/src/routes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const OngController = require("./controllers/ongController");
+const IncidentController = require("./controllers/incidentController");
+const ProfileController = require("./controllers/profileController");
+const SessionController = require("./controllers/sessionController");
+
+const routes = express.Router();
+
+routes.get("/ongs",OngController.index)
+routes.post("/ongs",OngController.create);
+
+routes.post("/sessions",SessionController.create)
+
+routes.get("/profile",ProfileController.index);
+
+routes.post("/incidents",IncidentController.create);
+routes.get("/incidents",IncidentController.index);
+routes.delete("/incidents/:id",IncidentController.delete);
+
+module.exports = routes;
